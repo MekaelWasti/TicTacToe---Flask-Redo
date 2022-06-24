a83a7121ea8eb8ca2d7f9106b222eb712c917743
Mekael Wasti <mekael_wasti@hotmail.com>
2022-06-24 09:41:08 -0400

    1

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def sendME():
 
 
 if __name__ == "__main__": 
-   app.run(debug=True, port="5000")
-   # app.run(debug=True,host="0.0.0.0", port="5000")
+   # app.run(debug=True, port="5000")
+   app.run(debug=False,host="0.0.0.0")
